3fa2c0b26b54b8c7900c1196f901b93f622cee36
akiroqw <akiroqw@gmail.com>
2023-04-15 23:26:13 +0300

    add TCPListener

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{ErrorKind, Read, Write};
+use std::net::TcpListener;
+use std::sync::mpsc;
+use std::thread;
+use chrono::{DateTime, Utc};
+
 fn main() {
-    println!("Hello, world!");
+
+    const LOCAL : &str = "127.0.0.1:6000";
+    const MESSAGE_SIZE : usize = 32;
+
+    let server : TcpListener = TcpListener::bind(LOCAL).expect("Failed to connect to the server!");
+    server.set_nonblocking(true).expect("Failed to initalize non-blocking");
+    
+
+
+
+    println!("The server is running: [{}]", Utc::now());
 }
